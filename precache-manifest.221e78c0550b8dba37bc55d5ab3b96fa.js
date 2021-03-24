986680c616ddf67b68605d3688f335a28848aa04
PatrickJayo <pj13sr@gmail.com>
2021-03-24 00:39:21 -0700

    Updates

---
+++ precache-manifest.221e78c0550b8dba37bc55d5ab3b96fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3689acaee3a73d022e2431d99bb64b57",
+    "url": "/aegis/index.html"
+  },
+  {
+    "revision": "c9fb8f4512c0425a166c",
+    "url": "/aegis/static/css/main.765194e3.chunk.css"
+  },
+  {
+    "revision": "15ce739a1922dcaf1c40",
+    "url": "/aegis/static/js/2.4d1b40c3.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/aegis/static/js/2.4d1b40c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9fb8f4512c0425a166c",
+    "url": "/aegis/static/js/main.21b3070f.chunk.js"
+  },
+  {
+    "revision": "6ce02dbccd10e6d5a043",
+    "url": "/aegis/static/js/runtime-main.8fe4ffb9.js"
+  }
+]);
