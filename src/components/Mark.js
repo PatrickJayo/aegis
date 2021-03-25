4ac3e9f9af3dac92b5d5ca3ae1f591c211da4918
PatrickJayo <pj13sr@gmail.com>
2021-03-25 00:00:52 -0700

    Added examples to documentation

--- src/components/Mark.js
+++ src/components/Mark.js
@@ -1,57 +1,134 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import atelierCaveDark from 'react-syntax-highlighter/dist/esm/styles/hljs/atelier-cave-dark';
 
-import './Mark.css';
-
-import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
-
-function Mark() {
+export default function Mark() {
   return (
     <div className="Mark">
-      <MarkdownViewer />
+      <SyntaxHighlighter language="Java" style={atelierCaveDark}>
+        {`
+
+##Basic Hello World function
+
+CLASS Example3:
+	helloWorld():
+  		OUTPUT("Hello World");
+	END
+END
+
+
+## Runs a loop to find and place all factors of y into an array
+## and prints the array
+
+CLASS Example1:
+    factors(NUM y):
+        NUM {} results;
+        NUM count = 0;
+        NUM x = 1;
+        LOOP(x <= y):
+            NUM z = y MOD x;
+            IF( z == 0 ):
+                results{count} = x;
+                count++;
+            END
+            x++;
+        END
+        OUTPUT( results );
+    END
+    factors(250);
+END
+
+
+CLASS Example2:
+  findMax(NUM i, NUM j) NUM:
+    IF( i > j ):
+      RETURN i;
+    ELSE:
+      RETURN j;
+    END
+  END
+END
+
+
+##Function to iteratively find the nth fibonacci number
+
+CLASS Example4:
+	iterativeFib(NUM n):
+  		NUM a = 0;
+        NUM b = 1;
+        DO(NUM i = 0, i < n, i++):
+        	NUM temp = a;
+            a = b;
+            b = temp + b;
+        END
+        RETURN a;
+	END
+END
+
+CLASS Example5:
+	iterativeFactorial(NUM n):
+  		NUM total = 1;
+        DO(NUM i = 1, i <= n, i++):
+        	total = total * i;
+        END
+        RETURN total;
+	END
+END
+
+
+
+CLASS ArraySorter:
+    len(NUM{} arr)NUM:
+        NUM count = 0;
+        LOOP(arr{count} != 0):
+            count++;
+        END
+        RETURN count;
+    END
+    bubbleSorter(NUM{} array) NUM{} :
+        NUM size = len( array );
+        DO(NUM i = 0, i < size-1, i++):
+            DO(NUM j = 0, j < size-i-1, j++):
+                IF(array{j} > array{j+1}):
+                    NUM{} temp = array{j};
+                    array{j} = array{j+1};
+                    array{j+1} = temp;
+                END
+            END
+        END
+    END
+END
+
+  
+##Function to recursively lists the 1st n fibonacci numbers
+
+CLASS Example7:
+    fibonacci(NUM n) NUM:
+        IF(n==0):
+            RETURN 0;
+        ELSEIF(n ==1  | n == 2):
+            RETURN 1;
+        ELSE:
+            RETURN fibonacci(n-1) + fibonacci(n-2) ;
+        END
+    END
+    main():
+        CHARS fibList = "";
+        NUM i = 0;
+        LOOP(i<n):
+            fibList = fibList + fibonacci(i)+ " ";
+            i++;
+        END
+        OUTPUT( fibList );
+    END
+    main();
+END
+        `}
+      </SyntaxHighlighter>
     </div>
   );
 }
 
-class MarkdownViewer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      content: null,
-      urlGitHubFile: 'https://patrickjayo.github.io/budgety/README.md',
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get(this.state.urlGitHubFile)
-      .then((response) => {
-        console.log(
-          'Success in fetching the file from ' + this.state.urlGitHubFile
-        );
-        this.setState({ content: response.data });
-      })
-      .catch((error) => {
-        console.err(
-          'Error in fetching the file from ' + this.state.urlGitHubFile
-        );
-      });
-  }
-
-  render() {
-    const { urlGitHubFile, content } = this.state;
-
-    return (
-      <div>
-        <h3> Fetched from: </h3> {urlGitHubFile}
-        <hr />
-        <p>
-          <ReactMarkdown source={content} />
-        </p>
-      </div>
-    );
-  }
-}
-
-export default Mark;
+const rootElement = document.getElementById('root');
+render(<Mark />, rootElement);
